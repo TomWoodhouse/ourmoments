453ae3828387a1d4ce47eb6b783ece54c8cb8b85
Tom Woodhouse <thomasawoodhouse@aol.com>
2022-07-19 02:16:37 +0100

    Updated connection details for server

--- Functions/dbconnection.php
+++ Functions/dbconnection.php
@@ -2,10 +2,15 @@
 
     function OpenCon()
     {
-        $dbhost = "localhost";
-        $dbuser = "root";
-        $dbpass = "ManUtd07";
-        $db = "Our Moments";
+        // $dbhost = "localhost";
+        // $dbuser = "root";
+        // $dbpass = "ManUtd07";
+        // $db = "Our Moments";
+
+        $dbhost = "db5009501952.hosting-data.io";
+        $dbuser = "dbu115237";
+        $dbpass = "ManUtd07!";
+        $db = "dbs8058133";
         $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
         
         return $conn;
